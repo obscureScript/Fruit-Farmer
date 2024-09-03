dd7d41e012149ed105cce5078fdd29293672843e
obscureScript <tuanzinho2@gmail.com>
2024-09-03 14:58:30 -0300

    Update loadUI.lua

--- loadUI.lua
+++ loadUI.lua
@@ -1,4 +1,4 @@
-getgenv().Team = "Pirates"
+CommF_:InvokeServer("SetTeam", "Marines")
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/obscureScript/Fruit-Farmer/main/source.lua'))()
 
 local Window = Library:CreateWindow('Obscure', 'Name', 'Visual UI Library', 'rbxassetid://10618928818', false, 'VisualUIConfigs', 'Default')
